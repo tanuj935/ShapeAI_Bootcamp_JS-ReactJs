06eca00094498a95b6b603c08ce5d74155e4a977
R TANUJ <52597749+tanuj935@users.noreply.github.com>
2021-06-05 23:03:23 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div class="note">
+      <h1>Javascript and reactJs</h1>
+      <p>
+        This class is a very interesting class in which i learnt React Js and
+        Javascript.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
